5e6f892de91689feb6cd3622143676f441b9ea62
Vana8000 <163187974+Vana8000@users.noreply.github.com>
2024-05-09 19:55:13 +0300

    Add files via upload

---
+++ StudentWithClock.java
@@ -0,0 +1,29 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+class Student {
+        public void learn() {
+                System.out.println("Я учусь. .zZ");
+                // Другие действия, связанные с учёбой
+        }
+}
+
+public class StudentWithClock extends Student {
+        @Override
+        public void learn() {
+                super.learn();
+                printCurrentTime();
+                System.out.println("Я закончил учиться");
+        }
+
+        private void printCurrentTime() {
+                LocalTime currentTime = LocalTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+                System.out.println("Текущее время: " + currentTime.format(formatter));
+        }
+
+        public static void main(String[] args) {
+                StudentWithClock student = new StudentWithClock();
+                student.learn();
+        }
+}
